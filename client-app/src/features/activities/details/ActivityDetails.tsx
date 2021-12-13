39d04552ac1811e22cf36fe9222c23b726d1dc26
Shane Blankenship <shane.blankenship32@hotmail.com>
2021-12-13 10:54:47 -0500

    end of section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Button, Card, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/Models/activities";
+import LoadingComponent from "../../../app/layout/loadingComponent";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
 
-}
+export default function ActivityDetails(){
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props){
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+
+    if(!activity) return <LoadingComponent content="Loading" />;
+
     return (
     <Card fluid>
         <Image src={`/assets/categoryImages/${activity.category}.jpg`}  />
@@ -27,7 +28,7 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
         <Card.Content extra>
             <Button.Group widths='2'>
                 <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit'/>
-                <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel'/>        
+                <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>        
             </Button.Group>
         </Card.Content>
     </Card>
